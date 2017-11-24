9324b1b52e56262e921ba734ddf67ac3ce135683
Silva <walteann.costs.silva@accenture.com>
2017-11-24 12:15:48 -0200

    Tela de ambiente fucionando OK

--- src/pages/ambiente-compras/ambiente-compras.ts
+++ src/pages/ambiente-compras/ambiente-compras.ts
@@ -1,115 +1,193 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
-import { ListaMercadoriaModels } from '../../modals/lista-mercadoria.models';
-
+import { Component, OnInit } from "@angular/core";
+import { NavController, NavParams, AlertController, ItemSliding } from "ionic-angular";
+import { ListaMercadoriaModels } from "../../modals/lista-mercadoria.models";
 
 @Component({
-	selector: 'page-ambiente-compras',
-	templateUrl: 'ambiente-compras.html',
+  selector: "page-ambiente-compras",
+  templateUrl: "ambiente-compras.html"
 })
 export class AmbienteComprasPage implements OnInit {
+  public listaDeMercadorias: ListaMercadoriaModels[];
 
-	public listaDeMercadorias: ListaMercadoriaModels[];
+  public precoTotal;
+  public Total: number;
 
-    public precoTotal;
-    public Total: number;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private _alertCtrl: AlertController
+  ) {}
 
-	constructor(public navCtrl: NavController, public navParams: NavParams,
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad AmbienteComprasPage");
+  }
 
-		private _alertCtrl: AlertController) {
+  ngOnInit() {
+    this.listaDeMercadorias = [
+      {
+        mercadoria: "Feijao",
+        qtd: 2,
+        precoUni: 1.0
+      },
+      {
+        mercadoria: "Arroz",
+        qtd: 2,
+        precoUni: 3.0
+      },
+      {
+        mercadoria: "Macarrao",
+        qtd: 5,
+        precoUni: 1.25
+      }
+    ];
+
+    this.precoTotal = 0.0;
+    this.Total = 0.0;
+    this.realizaCalculo();
+  }
+
+  alertProduto() {
+    var alert = this._alertCtrl.create({
+      title: "Adicionar Compra",
+      inputs: [
+        {
+          name: "mercadoria",
+          placeholder: "Produto",
+          type: "text"
+        },
+        {
+          name: "qtd",
+          placeholder: "quantidade",
+          type: "number"
+        },
+        {
+          name: "precoUni",
+          placeholder: "preco",
+          type: "number"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancelar",
+          role: "cancelar",
+          handler: data => {
+            console.log("Cancelar foi clicked");
+          }
+        },
+        {
+          text: "Adicionar",
+          role: "adicionar",
+          handler: data => {
+            this.listaDeMercadorias.push(data);
+            this.precoTotal = 0;
+            this.realizaCalculo();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  alertDeletar(produto, itemSli) {
+    var alert = this._alertCtrl.create({
+        title: "Remover Produto",
+        message: "Tem certeza que deseja remover " +produto.mercadoria+ " ?",
+        buttons: [
+            {
+                text: 'Não',
+                handler: () => {
+                    itemSli.close();
+                }
+            },
+            {
+                text: 'Sim',
+                handler: () =>  {
+                    this.removeuP(produto);
+
+                }
+            }
+        ]
+    });
+    alert.present();
+  }
+
+  alertProdutoUpdate(produto, itemSliR) {
+    var alert = this._alertCtrl.create({
+      title: "Alterar Compra",
+      inputs: [
+        {
+          name: "mercadoria",
+          placeholder: "Produto",
+          type: "text",
+          value: produto.mercadoria
+        },
+        {
+          name: "qtd",
+          placeholder: "quantidade",
+          type: "number",
+          value: produto.qtd
+        },
+        {
+          name: "precoUni",
+          placeholder: "preco",
+          type: "number",
+          value: produto.precoUni
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancelar",
+          role: "cancelar",
+          handler: data => {
+            itemSliR.close();
+          }
+        },
+        {
+          text: "Atualizar",
+          role: "atualizar",
+          handler: data => {
+            var index = this.listaDeMercadorias.indexOf(produto);
+            this.listaDeMercadorias[index].mercadoria = data.mercadoria;
+            this.listaDeMercadorias[index].qtd = data.qtd;
+            this.listaDeMercadorias[index].precoUni = data.precoUni;
+
+            this.precoTotal = 0;
+            this.realizaCalculo();
+
+            itemSliR.close();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 
 
-		}
+  addProduto() {
+    this.alertProduto();
+  }
 
-		ionViewDidLoad() {
-			console.log('ionViewDidLoad AmbienteComprasPage');
-		}
+  removerProduto(produto, itemSli: ItemSliding) {
+    this.alertDeletar(produto, itemSli);
+  }
 
+  removeuP(produto) {
+        this.listaDeMercadorias.splice(this.listaDeMercadorias.indexOf(produto), 1);
+        this.precoTotal = 0;
+        this.realizaCalculo();
 
+  }
 
-		ngOnInit() {
-			this.listaDeMercadorias = [
+  atualizarProduto(produto, itemSli: ItemSliding){
 
-				// {
-				// 	mercadoria: 'Feijao',
-				// 	qtd: 2,
-				// 	precoUni: 1.00
-				// },
-				// {
-				// 	mercadoria: 'Arroz',
-				// 	qtd: 2,
-				// 	precoUni: 3.00
-				// },
-				// {
-				// 	mercadoria: 'Macarrao',
-				// 	qtd: 5,
-				// 	precoUni: 1.25
-				// },
-			];
+    this.alertProdutoUpdate(produto, itemSli);
+  }
 
-            this.precoTotal = 0.0;
-            this.Total = 0.0;
-		this.realizaCalculo();
-	}
-
-
-	alertProduto() {
-		var alert = this._alertCtrl.create({
-			title: 'Adicionar Compra',
-			inputs: [
-				{
-					name: 'mercadoria',
-					placeholder: 'Produto',
-					type: 'text'
-				},
-				{
-					name: 'qtd',
-					placeholder: 'quantidade',
-					type: 'number'
-				},
-				{
-					name: 'precoUni',
-					placeholder: 'preco',
-					type: 'number'
-				}
-
-			],
-			buttons: [
-				{
-					text: 'Cancelar',
-					role: 'cancelar',
-					handler: data => {
-						console.log('Cancelar foi clicked');
-					}
-				},
-				{
-					text: 'Adicionar',
-					role: 'adicionar',
-					handler: data => {
-						this.listaDeMercadorias.push(data);
-						this.precoTotal = 0;
-						this.realizaCalculo();
-					}
-
-
-				}
-
-			]
-		});
-		alert.present();
-	}
-
-
-	addProduto() {
-		this.alertProduto();
-	}
-
-	realizaCalculo() {
-
-		for (let i = 0; i < this.listaDeMercadorias.length; i++) {
-			this.precoTotal += (this.listaDeMercadorias[i].precoUni * this.listaDeMercadorias[i].qtd);
-		}
-		this.Total = this.precoTotal.toFixed(2);
-	}
-
+  realizaCalculo() {
+    for (let i = 0; i < this.listaDeMercadorias.length; i++) {
+      this.precoTotal +=
+        this.listaDeMercadorias[i].precoUni * this.listaDeMercadorias[i].qtd;
+    }
+    this.Total = this.precoTotal.toFixed(2);
+  }
 }
